aff963fd4ac4b605b7356fb1ed38adb6149f95a9
Gilson Rocha Silva <gilson.silva@cinbesa.pmb.pa.gov.br>
2015-07-27 18:16:56 -0300

    Inclusão de certificado de sustentabilidade

---
+++ web/certificado_sustentabilidade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Certificado de Sustentabilidade</title>
+    <?php include 'head.php';?>
+</head>
+<body>
+<!--header-->
+<header class="clearfix indent">
+    <?php include 'topo.php';?>
+</header>
+<div class="global indent">
+    <!--content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 thumb-box4">
+                <h2 class="indent">Compromisso de Sustentabilidade da Produção Paraense</h2>
+                <div class="thumb-pad7 clearfix">
+                    <div class="extra-wrap">
+                        <div class="thumbnail">
+                            <div class="caption">
+                                <p> A MEJER enquadrou-se nos requisitos e procedimentos operacionais quanto ao compromisso de responsabilidade sócio - econômica - ambiental estabelecido pelo Conselho de Agronegócio do Estado do Pará.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <img src="img/certificado_sustentabilidade.jpg" width="100%">
+            </div>
+        </div>
+    </div>
+
+</div>
+<!--footer-->
+<?php include 'footer.php';?>
+</body>
+</html>
